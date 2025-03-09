e34d3fce2f982e0d679350c0b1b5da143bcc35ef
MRI-Batavia <muhammadrezaitok@gmail.com>
2025-03-09 16:01:04 +0700

    updated all files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// ini versi 1 java
+// this is javascript file
+document.addEventListener("DOMContentLoaded", function () {
+    function scrollToTop() {
+        let sound = document.getElementById('soundEffect');
+        sound.volume = 0.15; // Adjust volume (0.0 = mute, 1.0 = full volume)
+        sound.play();
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    // Assign scrollToTop function to button click
+    document.getElementById("backToTop").addEventListener("click", scrollToTop);
+
+    // Show or hide the button based on scroll position
+    window.onscroll = function () {
+        var button = document.getElementById("backToTop");
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    };
+});
